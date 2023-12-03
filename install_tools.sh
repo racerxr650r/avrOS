19c59c42dcd801c15295be52529609978b4e7857
John Anderson <129009094+racerxr650r@users.noreply.github.com>
2023-12-03 11:15:49 -0700

    Update install_tools.sh
    Removed GUI tools

--- install_tools.sh
+++ install_tools.sh
@@ -1,21 +1,12 @@
 #! /bin/bash
-
+# Update apt-get to the latest in the repositories
 sudo apt-get update
 
 # Install make, git, gcc-avr, gcc tools, and unzip
-sudo apt-get install -y make binutils gcc-avr gdb-avr avr-libc flex byacc bison cmake unzip 
+sudo apt-get install -y make binutils gcc-avr gdb-avr avr-libc flex byacc bison cmake unzip avrdude
 
 # Install command line app tools
-sudo apt-get install -y minicom screens micro 
-
-# Install visual tools
-sudo apt-get install -y gtkterm geany geany-plugins git-cola meld code
-
-# Get geany color scheme(s)
-wget -P ~/.config/geany/colorschemes https://raw.github.com/geany/geany-themes/master/colorschemes/delt-dark.conf
-wget -P ~/.config/geany/colorschemes https://raw.github.com/geany/geany-themes/master/colorschemes/dark-colors.conf
-wget -P ~/.config/geany/colorschemes https://raw.github.com/geany/geany-themes/master/colorschemes/himbeere.conf
-wget -P ~/.config/geany/colorschemes https://raw.github.com/geany/geany-themes/master/colorschemes/mc.conf
+sudo apt-get install -y minicom screens micro tree devmon neovim 
 
 # Get the AVR Dx DFP file for the AVR DA libs and header files
 wget http://packs.download.atmel.com/Atmel.AVR-Dx_DFP.2.2.253.atpack
@@ -28,8 +19,8 @@ rm -rf Atmel.AVR-Dx_DFP.2.2.253
 rm Atmel.AVR-Dx_DFP.2.2.253.atpack
 
 # Get the latest avrdude source, build it, and install it
-./install_avrdude.sh
-rm -rf ../avrdude
+#./install_avrdude.sh
+#rm -rf ../avrdude
 
 # Goto the avrOS_example directory and build the application image (.hex)
 pushd .
