03e13d462d5af62d01099f49e7fa7cf28be2a7fd
racerxr650r <racerxr650r@gmail.com>
2024-03-03 20:55:18 -0700

    fixed path in install_all_tools script

--- util/scripts/install_all_tools.sh
+++ util/scripts/install_all_tools.sh
@@ -9,7 +9,7 @@ if [ -z "$AVROSHOME" ]; then
     echo "Goto the avrOS root directory and the run this command: export AVROSHOME=$(pwd)"
 else
     # Install the command line tools
-    ./install_cli_tools.sh
+    ${AVROSHOME}/util/scripts/install_cli_tools.sh
     # Install the graphical tools
-    ./install_gui_tools.sh
+    ${AVROSHOME}/util/scripts/install_gui_tools.sh
 fi
